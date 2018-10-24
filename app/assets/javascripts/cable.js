98473fd9f56e26d31686b732d6355f34e5803cfa
Andy Stein <andy@steinonline.net>
2018-10-23 22:55:11 -0400

    Implement basic login

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,6 +1,9 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
-//
+//= require jquery
+//= require bootstrap
+//= require rails-ujs
+//= require turbolinks
 //= require action_cable
 //= require_self
 //= require_tree ./channels
