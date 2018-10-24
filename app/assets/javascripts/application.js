14f00a33a01267616dc59fab11270a5b05d18ff9
Andy Stein <andy@steinonline.net>
2018-10-24 11:56:00 -0400

    Another try to fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require chosen-jquery
 //= require bootstrap
 //= require bootstrap-sprockets
 //= require turbolinks
